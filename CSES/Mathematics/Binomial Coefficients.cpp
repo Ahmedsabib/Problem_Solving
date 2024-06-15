64c1e87a45167bd3702de646347d0e9608ea3a7f
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2024-06-15 16:34:33 +0600

    Update Binomial Coefficients.cpp

--- CSES/Mathematics/Binomial Coefficients.cpp
+++ CSES/Mathematics/Binomial Coefficients.cpp
@@ -7,7 +7,7 @@ using namespace std;
 const int maxn = 1e6 + 5;
 const int mod = 1e9 + 7;
 
-int fact[maxn];
+int fact[maxn], inv[maxn];
 
 int binpow(int a, int b, int m) {
   a %= m;
@@ -22,23 +22,34 @@ int binpow(int a, int b, int m) {
   return res;
 }
 
-int mod_inv(int a, int m) { return binpow(a, m - 2, m); }
-
-int32_t main() {
-  ios_base::sync_with_stdio(0), cin.tie(0);
-
+void factors() {
   fact[0] = fact[1] = 1;
   for (int i = 2; i < maxn; ++i) {
     fact[i] = fact[i - 1]%mod * i;
     fact[i] %= mod;
   }
+}
+
+void mod_inv() {
+  inv[maxn - 1] = binpow(fact[maxn - 1], mod - 2, mod);
+  for (int i = maxn - 1; i >= 1; --i) {
+    inv[i - 1] = inv[i]%mod * i;
+    inv[i - 1] %= mod;
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0), cin.tie(0);
+
+  factors();
+  mod_inv();
 
   int tests;
   cin >> tests;
   for (int cases = 0; cases < tests; ++cases) {
     int a, b;
     cin >> a >> b;
-    cout << ((fact[a] * mod_inv(fact[b] * fact[a - b], mod)))%mod << '\n';
+    cout << (fact[a] * inv[b]%mod * inv[a - b]%mod)%mod << '\n';
   }
   return 0;
 }
